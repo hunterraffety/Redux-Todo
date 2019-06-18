6bd1d87b8d5dec893ef45b218cf59d474aa53f36
hsrsbd <hsraffety@gmail.com>
2019-06-18 11:28:48 -0700

    initial commit i guess

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,9 @@
 // dependencies
 import React from 'react';
 
+// components
+import Form from './components/Form/Form';
+
 // styles
 import './App.css';
 
@@ -8,10 +11,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        Learn React
+        <Form />
       </header>
     </div>
   );
